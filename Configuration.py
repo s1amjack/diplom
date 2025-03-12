638071fa0151640d23dd396e13936ab792e0df29
s1amjack <polunin-d@mail.ru>
2025-03-12 17:54:33 +0300

    Configuration.py

---
+++ Configuration.py
@@ -0,0 +1,11 @@
+# Дени Полунин, 27 когорта — Финальный проект. Инженер по тестированию плюс
+
+# URL_SERVICE хранит базовый URL веб-сервиса "Самокат"
+
+URL_SERVICE = "https://d6dd0ec0-e11b-4bad-b48b-60e65d4bec86.serverhub.praktikum-services.ru"
+
+# Путь к API-методу POST-запроса, для создания заказа.
+CREATE_ORDER_PATH = "/api/v1/orders"
+
+# Путь к API-методу GET-запроса, на получения заказа по треку заказа.
+GET_ORDER_WITHIN_TRACK_PATH = "/api/v1/orders/track"
